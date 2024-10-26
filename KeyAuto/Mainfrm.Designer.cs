cb93a8e00a5275b6230a2c16e0f844034ab0ab94
zhuweiye <61441059+Antruly@users.noreply.github.com>
2024-10-27 02:07:36 +0800

    Add files via upload
    第一次提交自动按键触发器

---
+++ KeyAuto/Mainfrm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace KeyAuto
+{
+    partial class Mainfrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(47, 189);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(172, 189);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(172, 35);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(57, 21);
+            this.txtKey.TabIndex = 2;
+            this.txtKey.Text = "S";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "键位";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "时间间隔（ms）";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(172, 82);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(57, 21);
+            this.txtTime.TabIndex = 4;
+            this.txtTime.Text = "2000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "按键次数";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(172, 133);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(57, 21);
+            this.txtNumber.TabIndex = 6;
+            this.txtNumber.Text = "100000";
+            // 
+            // Mainfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 244);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Mainfrm";
+            this.Text = "自动键位触发器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
+
---
+++ KeyAuto/Mainfrm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+
+
+namespace KeyAuto
+{
+    public partial class Mainfrm : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        delegate void NumberDelegate(int nb);
+        delegate void VoidType(object sender, EventArgs e);
+
+        
+
+        /// <summary>
+        /// 按下
+        /// </summary>
+        const uint KEYEVENTF_EXTENDEDKEY = 0x1;
+        /// <summary>
+        /// 抬起
+        /// </summary>
+        const uint KEYEVENTF_KEYUP = 0x2;
+        const byte VK_CONTROL = 0x11;
+        const byte VK_C = 0x43;
+
+
+        Thread _workThread = null;
+
+        bool _isStop = false;
+
+        // 将单个字符转换为对应的VK码  
+        static byte CharToVkCode(char c)
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                return (byte)(0x41 + (c - 'A')); // A的VK码是0x41，之后依次加1  
+            }
+            else if (c >= 'a' && c <= 'z')
+            {
+                // 小写字母的VK码与大写字母相同，因此我们可以直接使用大写字母的逻辑  
+                return (byte)(0x41 + (c - 'a')); // 或者使用(c - 'A' + 0x41) & 0xFF来确保结果是byte类型  
+            }
+            else if (c >= '0' && c <= '9')
+            {
+                return (byte)(0x30 + (c - '0')); // 0的VK码是0x30，之后依次加1  
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void test()
+        {
+
+            // 模拟按下 Ctrl 键  
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_EXTENDEDKEY, UIntPtr.Zero);
+
+            // 模拟按下 C 键  
+            keybd_event(VK_C, 0, KEYEVENTF_EXTENDEDKEY, UIntPtr.Zero);
+
+            // 模拟松开 C 键  
+            keybd_event(VK_C, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+
+            // 模拟松开 Ctrl 键  
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+        }
+
+        public Mainfrm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (_workThread != null)
+            {
+                return;
+            }
+            _isStop = false;
+            txtKey.Enabled = false;
+            txtTime.Enabled = false;
+            txtNumber.Enabled = false;
+
+            _workThread = new Thread(runKey);
+
+            _workThread.IsBackground = true;
+            _workThread.Start();
+        }
+
+        private void runKey()
+        {
+
+
+            string strKey = txtKey.Text;
+            int nTime = Int32.Parse(txtTime.Text);
+            int nNumber = Int32.Parse(txtNumber.Text);
+
+            byte bKey = CharToVkCode(strKey[0]);
+
+            if (bKey == 0) return;
+
+
+            while (!_isStop)
+            {
+                
+                // 模拟按下
+                keybd_event(bKey, 0, KEYEVENTF_EXTENDEDKEY, UIntPtr.Zero);
+                Random rd = new Random(DateTime.Now.Second);
+                Thread.Sleep(100 + rd.Next(200));
+                // 模拟松开
+                keybd_event(bKey, 0, KEYEVENTF_KEYUP, UIntPtr.Zero);
+
+                nNumber--;
+
+                NumberDelegate dlg = new NumberDelegate(updateNumber);
+                this.Invoke(dlg, nNumber);
+               
+                if (nNumber <= 0)
+                {
+                    nNumber = 0;
+                    VoidType vd = new VoidType(btnStop_Click);
+                    this.Invoke(vd, null, null);
+                    return;
+                }
+                
+                int unRunTime = nTime;
+                int runTime = 0;
+                while (unRunTime > 0 && !_isStop)
+                {
+
+                    if(unRunTime > 100){
+                        unRunTime -= 100;
+                        runTime = 100;
+                    }
+                    else
+                    {
+                        unRunTime = 0;
+                        runTime = unRunTime;
+                    }
+                    Thread.Sleep(runTime);
+                }
+               
+            }
+
+        }
+
+        private void updateNumber(int nb)
+        {
+            txtNumber.Text = nb.ToString();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            _isStop = true;
+
+            if (sender != null)
+            _workThread.Join();
+
+            _workThread = null;
+            txtKey.Enabled = true;
+            txtTime.Enabled = true;
+            txtNumber.Enabled = true;
+        }
+
+    }
+}
